274798dd272abe3e7bdf474834847b644ceb785f
izuutech <izutechsofficial@gmail.com>
2023-08-10 10:39:46 +0100

    h

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Register = () => {
-    return (
-        <div>
+  return <div>hello</div>;
+};
 
-        </div>
-    )
-}
-
-export default Register
+export default Register;
