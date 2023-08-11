48b75677d2c7a411204e64ef8cff59a05a0293da
izuutech <izutechsofficial@gmail.com>
2023-08-11 21:40:06 +0100

    rember stuff

--- src/Components/login/login-box.jsx
+++ src/Components/login/login-box.jsx
@@ -27,6 +27,15 @@ export default function LoginBox() {
           className="border border-gray-light outline-none rounded text-sm w-full py-3 px-3 leading-tight focus:outline-none"
         />
       </div>
+      <div className="flex justify-between my-2 items-center">
+        <div className="flex">
+          <input type="checkbox" className="mr-2" />
+          <div className="">Remember me</div>
+        </div>
+        <a href="#" className="text-blue-300">
+          Lost you password?
+        </a>
+      </div>
       <button
         type="submit"
         className={`bg-green-500 text-white w-full rounded text-white-primary text-sm py-3 font-medium bg-purple-primary grid place-items-center cursor-pointer 
@@ -36,7 +45,7 @@ export default function LoginBox() {
         }
         `}
       >
-        Create Account
+        Login
       </button>
       <div className="my-8 flex items-center justify-between">
         <div className="w-[45%] h-[2px] bg-gray-200"></div>
