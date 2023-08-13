e962983c07df6d3cd0f1e6a3b007cd4fb9f60f89
izuutech <izutechsofficial@gmail.com>
2023-08-14 00:23:39 +0100

    zeitos text

--- src/Components/login/login-box.jsx
+++ src/Components/login/login-box.jsx
@@ -1,5 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 export default function LoginBox() {
   return (
     <div className="bg-white w-full pb-8 px-4 mb-8">
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -7,6 +7,9 @@ const Register = () => {
       <div className="flex shadow">
         <div className="w-[0%] md:w-[50%] relative">
           <div className="absolute bg-[#0259DB] w-[100%] h-[100%] opacity-50"></div>
+          <div className="absolute text-white font-bold left-8 top-8 hidden md:block">
+            zietios
+          </div>
           <img src="/hand.png" alt="hand" className="w-[100%] h-[100%]" />
         </div>
         <div className="w-[100%] ml-[0%] sm:w-[70%] sm:ml-[15%] md:ml-[0%] md:w-[50%]  bg-white py-8">
