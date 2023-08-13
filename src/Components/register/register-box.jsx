7a96b390f4c287aa4c22826655d9cee7a87e8583
izuutech <izutechsofficial@gmail.com>
2023-08-14 00:15:43 +0100

    redo register

--- src/Components/register/register-box.jsx
+++ src/Components/register/register-box.jsx
@@ -2,69 +2,97 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function RegisterBox() {
   return (
-    <div className="bg-white ml-[20%] w-[60%] w rounded-md py-8 px-4 my-8">
-      <div className="font-bold text-xl">Let's create your account</div>
-      <div className="mt-4 mb-8">
-        Already have an account?{" "}
-        <a href="/login" className="text-blue-300">
-          Log in
-        </a>{" "}
+    <div className="bg-white w-full pb-8 px-4 mb-8">
+      <div className="flex flex-col sm:flex-row flex-wrap justify-between">
+        <div className="my-4 w-[100%] md:w-[45%]">
+          <div className="font-medium mb-2">First Name</div>
+          <input
+            type="text"
+            className="border border-gray-light outline-none rounded text-sm w-full py-3 px-3 leading-tight focus:outline-none"
+          />
+        </div>
+        <div className="my-4 w-[100%] md:w-[45%]">
+          <div className="font-medium mb-2">Last Name</div>
+          <input
+            type="text"
+            className="border border-gray-light outline-none rounded text-sm w-full py-3 px-3 leading-tight focus:outline-none"
+          />
+        </div>
+        <div className="my-4 w-[100%] md:w-[45%]">
+          <div className="font-medium mb-2">Phone Number</div>
+          <input
+            type="text"
+            className="border border-gray-light outline-none rounded text-sm w-full py-3 px-3 leading-tight focus:outline-none"
+          />
+        </div>
+        <div className="my-4 w-[100%] md:w-[45%]">
+          <div className="font-medium mb-2">Email Address</div>
+          <input
+            type="text"
+            className="border border-gray-light outline-none rounded text-sm w-full py-3 px-3 leading-tight focus:outline-none"
+          />
+        </div>
+        <div className="my-4 w-[100%] md:w-[45%]">
+          <div className="font-medium mb-2">Password</div>
+          <input
+            type="password"
+            placeholder="*****"
+            className="border border-gray-light outline-none rounded text-sm w-full py-3 px-3 leading-tight focus:outline-none"
+          />
+        </div>
+        <div className="my-4 w-[100%] md:w-[45%]">
+          <div className="font-medium mb-2">Confirm Password</div>
+          <input
+            type="password"
+            placeholder="*****"
+            className="border border-gray-light outline-none rounded text-sm w-full py-3 px-3 leading-tight focus:outline-none"
+          />
+        </div>
+        <div className="my-4 w-[100%] md:w-[45%]">
+          <div className="font-medium mb-2">Category</div>
+          <select className="border border-gray-light outline-none rounded bg-white text-sm w-full py-3 px-3 leading-tight focus:outline-none">
+            <option>Hello</option>
+          </select>
+        </div>
+        <div className="my-4 w-[100%] md:w-[45%]">
+          <div className="font-medium mb-2">Skills</div>
+          <select className="border border-gray-light outline-none rounded bg-white text-sm w-full py-3 px-3 leading-tight focus:outline-none">
+            <option>Hello</option>
+          </select>
+        </div>
       </div>
-      <div className="my-4">
-        <div className="font-medium mb-2">Display Name</div>
-        <input
-          type="text"
-          className="border border-gray-light outline-none rounded text-sm w-full py-3 px-3 leading-tight focus:outline-none"
-        />
-      </div>
-      <div className="my-4">
-        <div className="font-medium mb-2">Username</div>
-        <input
-          type="text"
-          className="border border-gray-light outline-none rounded text-sm w-full py-3 px-3 leading-tight focus:outline-none"
-        />
-      </div>
-      <div className="my-4">
-        <div className="font-medium mb-2">Email</div>
-        <input
-          type="text"
-          className="border border-gray-light outline-none rounded text-sm w-full py-3 px-3 leading-tight focus:outline-none"
-        />
-      </div>
-      <div className="my-4">
-        <div className="font-medium mb-2">Password</div>
-        <input
-          type="text"
-          className="border border-gray-light outline-none rounded text-sm w-full py-3 px-3 leading-tight focus:outline-none"
-        />
+      <div className="flex justify-between my-4 items-center">
+        <div className="flex">
+          <input type="checkbox" className="mr-2" />
+          <div className="">
+            I agree to all{" "}
+            <a href="/login" className="text-[#0259DB]">
+              Terms
+            </a>{" "}
+            and{" "}
+            <a href="/login" className="text-[#0259DB]">
+              Privacy Policy
+            </a>
+          </div>
+        </div>
       </div>
       <button
         type="submit"
-        className={`bg-green-500 text-white w-full rounded text-white-primary text-sm py-3 font-medium bg-purple-primary grid place-items-center cursor-pointer 
+        className={`bg-[#0259DB] text-white w-full rounded text-white-primary text-sm py-3 font-medium bg-purple-primary grid place-items-center cursor-pointer 
         ${
           ""
           //   loading ? "cursor-not-allowed opacity-60" : "cursor-pointer"
         }
         `}
       >
-        Create Account
+        Register
       </button>
-      <div className="my-8 flex items-center justify-between">
-        <div className="w-[45%] h-[2px] bg-gray-200"></div>
-        <div>OR</div>
-        <div className="w-[45%] h-[2px] bg-gray-200"></div>
-      </div>
-      <div className="flex justify-between">
-        <div className="bg-white border-2 border-blue-300 text-white w-[30%] rounded text-blue-300 text-xs py-3 font-medium bg-purple-primary grid place-items-center cursor-pointer">
-          Continue Facebook
-        </div>
-        <div className="flex bg-white border-2 border-red-500 text-white w-[30%] rounded text-red-500 text-xs py-3 font-medium bg-purple-primary grid place-items-center cursor-pointer">
-          <FontAwesomeIcon icon="fa-facebook" className="text-red-700" />
-          Continue Google
-        </div>
-        <div className="bg-white border-2 border-gray-700 text-white w-[30%] rounded text-black text-xs py-3 font-medium bg-purple-primary grid place-items-center cursor-pointer">
-          Continue Apple
-        </div>
+      <div className="w-full flex flex-row justify-center my-4">
+        Have an existing account?{" "}
+        <a href="/login" className="text-[#0259DB]">
+          {" "}
+          Login
+        </a>
       </div>
     </div>
   );
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -4,16 +4,19 @@ import RegisterBox from "../Components/register/register-box";
 
 const Register = () => {
   return (
-    <div className="bg-pink-100 text-black pb-8">
-      <Navbar />
-      <div className="">
-        <div className="text-2xl font-bold  w-[100%] flex flex-col items-center">
-          Register
+    <div className="text-black px-8 py-8 w-[100%] bg-[url('/regBg.png')]">
+      <div className="flex shadow">
+        <div className="w-[0%] md:w-[50%] relative">
+          <div className="absolute bg-[#0259DB] w-[100%] h-[100%] opacity-50"></div>
+          <img src="/hand.png" alt="hand" className="w-[100%] h-[100%]" />
         </div>
-        <div className="w-[100%] flex flex-col items-center">
-          Give your visitor a smooth online experience wuth a solid UX design
+        <div className="w-[100%] ml-[0%] sm:w-[70%] sm:ml-[15%] md:ml-[0%] md:w-[50%]  bg-white py-8">
+          <div className="text-2xl font-bold  w-[100%] flex flex-col items-center">
+            Register
+          </div>
+          <div className="w-[100%] flex flex-col items-center">As a client</div>
+          <RegisterBox />
         </div>
-        <RegisterBox />
       </div>
     </div>
   );
